d0a44a01ea28b8698135f85f18ea877d828557c2
Gyani <rgyani48@gmail.com>
2026-09-06 15:45:12 +0530

    fix: switch from BrowserRouter to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <-- BrowserRouter ki jagah HashRouter import kiya
 
 import App from "./App.jsx";
 import AuthProvider from "./store/auth-context.jsx";
@@ -10,12 +10,12 @@ import './App.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* <-- Yahan bhi BrowserRouter ki jagah HashRouter kar diya */}
       <AuthProvider>
         <UserProvider>
           <App />
         </UserProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
