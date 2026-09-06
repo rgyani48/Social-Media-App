bce25ca714da6bd2b819b3e1f0ae0818f36c7473
Gyani <rgyani48@gmail.com>
2026-09-06 14:28:19 +0530

    fix: update vite config and firebase settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Social-Media-App/",
-});
+  base: '/Social-Media-App/', // Yeh hona zaroori hai GitHub Pages ke liye
+})
