8542168a526907ca161bbf5e46774400948bb491
Gyani <rgyani48@gmail.com>
2026-09-06 14:34:47 +0530

    build: update production build for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Social-Media-App/', // Yeh hona zaroori hai GitHub Pages ke liye
+  base: './',
 })
